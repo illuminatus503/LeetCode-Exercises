dae514bc3769bdd50e26878c1be6224e8ca00eb7
illuminatus503 <22548652+illuminatus503@users.noreply.github.com>
2025-05-02 14:22:19 +0200

    CI configurarion patch. PEP compatibility

--- src/easy/two_sum.py
+++ src/easy/two_sum.py
@@ -1,7 +1,9 @@
 """Two Sum
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+Given an array of integers nums and an integer target, return indices of the two 
+numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You may assume that each input would have exactly one solution, and you may not 
+use the same element twice.
 
 You can return the answer in any order.
 
@@ -33,12 +35,12 @@ Only one valid answer exists.
 Follow-up: Can you come up with an algorithm that is less than O(n^2) time complexity?
 """
 
-from typing import List
+from typing import Dict, List, Optional
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        to_index = {}
+    def twoSum(self, nums: List[int], target: int) -> Optional[List[int]]:
+        to_index : Dict[int, int] = {}
 
         for i, number in enumerate(nums):
             # The answer is composed by two elements. So, 
